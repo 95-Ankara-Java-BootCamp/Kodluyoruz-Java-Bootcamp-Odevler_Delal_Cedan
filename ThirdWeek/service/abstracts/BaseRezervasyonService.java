684d19935f183acf963d961fa30a1b573be0fe21
DelalCedan <delal.cedan.1994@gmail.com>
2021-07-17 09:39:24 +0300

    commit1

---
+++ ThirdWeek/service/abstracts/BaseRezervasyonService.java
@@ -0,0 +1,7 @@
+package ThirdWeek.service.abstracts;
+
+import ThirdWeek.model.Ucak;
+
+public interface BaseRezervasyonService {
+    void rezervasyonAl(Ucak ucak);
+}
---
+++ ThirdWeek/service/concretes/THYRezervasyon.java
@@ -0,0 +1,30 @@
+package ThirdWeek.service.concretes;
+
+import ThirdWeek.model.Ucak;
+import ThirdWeek.service.abstracts.BaseRezervasyonService;
+
+import java.util.Scanner;
+
+public class THYRezervasyon implements BaseRezervasyonService {
+    private int koltukNumarasi;
+
+    @Override
+    public void rezervasyonAl(Ucak ucak) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Koltik Tipi Seçiniz?");
+        System.out.println("1- Business \n2- Ekonomi");
+        int secim = scanner.nextInt();
+
+        System.out.println(ucak.getKoltukTuru());
+        while (true){
+            koltukNumarasi = scanner.nextInt();
+            boolean koltukDoluluk = ucak.getKoltuklar().get(koltukNumarasi);
+            if(!koltukDoluluk){
+                ucak.getKoltuklar().put(koltukNumarasi,true);
+                System.out.println(ucak.getKoltuklar() + "\n" + ucak.getKoltukTuru());
+            }else{
+                System.out.println("Koltuk malesef dolu!");
+            }
+        }
+    }
+}
